af4450433d0389e6bca65211a26eb651b5cdfbea
Sri-Hasnika <srihasnika@gmail.com>
2025-03-19 15:37:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,7 @@ export default defineConfig({
     host: "0.0.0.0", // Allow external access
     port: 10000, // Render assigns a port, but you can set a default
   },
+  preview: {
+    allowedHosts: ["focusflow-750n.onrender.com"], // Allow Render host
+  },
 })
